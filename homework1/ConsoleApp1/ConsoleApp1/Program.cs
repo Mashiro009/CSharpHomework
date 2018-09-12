d6c201f1d5acfcbe69bbbb758d5de30cf0bf81ba
Mashiro <Mashiro@example.com>
2018-09-12 12:15:35 +0800

    First Chapter

---
+++ homework1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int x = 0;
+            int y = 0;
+            Console.Write("Please input an int:");
+            s = System.Console.ReadLine();
+            x = Int32.Parse(s);
+            Console.Write("Please input an int:");
+            s = System.Console.ReadLine();
+            y = Int32.Parse(s);
+            Console.WriteLine(x * y);
+        }
+    }
+}
