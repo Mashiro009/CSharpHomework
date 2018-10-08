c46971325cb5ecfa4836b269cf2c03a9b472ee46
Mashiro <Mashiro@example.com>
2018-10-08 20:51:57 +0800

    第四次作业

---
+++ homework4/program1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace program1
+{
+    //声明事件参数类型
+    public class ClockEventArgs : EventArgs
+    {
+
+    }
+
+    //声明委托类型
+    public delegate void ClockEventHandler(object sender, ClockEventArgs e);
+    //定义闹钟
+    public class AlarmClock
+    {
+        //声明事件
+        public event ClockEventHandler Alarm;
+
+        public void DoAlarm()
+        {
+            if (Alarm != null)
+            {
+                ClockEventArgs clock = new ClockEventArgs();
+                Alarm(this, clock);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Alarm = input();
+            DateTime now = DateTime.Now;
+            //Console.WriteLine("现在是:" + now.ToShortTimeString().ToString());
+            while (now < Alarm) 
+            {
+                Thread.Sleep(6000);//六秒刷新一次
+                now = DateTime.Now;
+                //Console.WriteLine("现在是:" + now.ToShortTimeString().ToString());
+            }
+            AlarmClock alarmClock = new AlarmClock();
+            alarmClock.Alarm += alarm;
+            alarmClock.DoAlarm();
+            Console.ReadKey();
+        }
+
+        public static void alarm(object sender, ClockEventArgs e)
+        {
+            Console.WriteLine("Ring!!!!");
+        }
+
+        public static DateTime input()
+        {
+
+            Console.Write("请输入闹钟的小时：");
+            int h = Int32.Parse(Console.ReadLine());
+            while (h < 0 || h > 23)
+            {
+                Console.Write("你输入有误请重新输入小时：");
+                h = Int32.Parse(Console.ReadLine());
+            }
+            Console.Write("请输入闹钟的分钟：");
+            int min = Int32.Parse(Console.ReadLine());
+            while (min < 0 || min > 59)
+            {
+                Console.Write("你输入有误请重新输入分钟：");
+                min = Int32.Parse(Console.ReadLine());
+            }
+            string str = h.ToString() + ":" + min.ToString();
+            DateTime dateTime = new DateTime(DateTime.Now.Year,
+                DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
+            dateTime += new TimeSpan(h, min, 0);
+            Console.WriteLine("闹钟的时间是：" + dateTime.ToShortTimeString().ToString());
+            return dateTime;
+        }
+
+    }
+    
+}
---
+++ homework4/program2/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    class Order
+    {
+        public int id { get; set; }
+        public List<OrderDetails> orderList = new List<OrderDetails>();
+        public Order(int id)
+        {
+            this.id = id;
+        }
+        public void addOrderDetails(OrderDetails orderDetails)
+        {
+            orderList.Add(orderDetails);
+        }
+        public void removeOrderDetails(OrderDetails orderDetails)
+        {
+            try
+            {
+                orderList.Remove(orderDetails);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("订单内容为空，无法删除");
+            }
+
+        }
+        public int findOrderDetailsByString(string str)
+        {
+            for (int i = 0; i < orderList.Count; i++)
+            {
+                if (orderList[i].pairs.ContainsValue(str))
+                    return i;
+            }
+            return -1;
+        }
+        public void showOrderDetails()
+        {
+            for (int i = 0; i < orderList.Count; i++)
+            {
+                Console.WriteLine("订单号：" + orderList[i].pairs[0]
+                    + " 客人：" + orderList[i].pairs[1] + " 商品：" + orderList[i].pairs[2]);
+            }
+        }
+    }
+
+    class OrderService
+    {
+        public static void addOrder(List<Order> list,Order order)
+        {
+            list.Add(order);
+        }
+
+        public static void removeOrder(List<Order> list, Order order)
+        {
+            try
+            {
+                list.Remove(order);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("订单为空无法删除");
+            }
+        }
+        public static int findOrderByString(List<Order> list, string str)
+        {
+            for (int i = 0; i < list.Count; i++) 
+            {
+                foreach(OrderDetails orderDetails in list[i].orderList)
+                {
+                    if(orderDetails.pairs.ContainsValue(str))
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        public static void changeOrder(List<Order> list,
+            int i, int x, int y, string str)//修改i个订单第x行第y个属性
+        {
+            try
+            {
+                list[i].orderList[x].pairs[y] = str;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("出现错误无法修改");
+            }
+        }
+    }
+
+    class OrderDetails
+    {
+        public Dictionary<int,string> pairs = new Dictionary<int, string>();
+        public OrderDetails(string id,string custom,string goods)
+        {
+            pairs[0] = id;
+            pairs[1] = custom;
+            pairs[2] = goods;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Order> orders = new List<Order>();
+            //初始化订单订单
+            OrderService.addOrder(orders, new Order(1));
+            OrderService.addOrder(orders, new Order(2));
+            orders[0].addOrderDetails(new OrderDetails("001", "mayun", "Taobao"));
+            orders[0].addOrderDetails(new OrderDetails("002", "mahuateng", "Tecent"));
+            orders[1].addOrderDetails(new OrderDetails("007","wangjianlin","Wanda"));
+            //删除第二个订单
+            OrderService.removeOrder(orders, orders[1]);
+            //查找订单
+            int i = OrderService.findOrderByString(orders, "mayun");
+            int x = orders[i].findOrderDetailsByString("mayun");
+            //修改订单
+            OrderService.changeOrder(orders, i, x, 2, "huichuangali");
+            //展示订单
+            foreach(Order order in orders)
+            {
+                Console.WriteLine("订单" + order.id);
+                order.showOrderDetails();
+            }
+            Console.ReadKey();
+        }
+    }
+}
