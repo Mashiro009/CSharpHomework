b04eeb7eb29a51ef8d033373456b2f36e85a875c
Mashiro <Mashiro@example.com>
2018-10-24 18:12:49 +0800

    第六次作业

---
+++ homework6/OrderTest/OrderService.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace ordertest {
+    /// <summary>
+    /// OrderService:provide ordering service,
+    /// like add order, remove order, query order and so on
+    /// 实现添加订单、删除订单、修改订单、查询订单（按照订单号、商品名称、客户等字段进行查询)
+    /// </summary>
+    public class OrderService { 
+
+        private Dictionary<uint, Order> orderDict;
+        public Dictionary<uint, Order> Dict
+        {
+            get { return orderDict; }
+        }
+        /// <summary>
+        /// OrderService constructor
+        /// </summary>
+        public OrderService() {
+            orderDict = new Dictionary<uint, Order>();
+        }
+
+        public void XmlSerializeExport(XmlSerializer xmlser,string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create);
+            //foreach(Order order in orderDict.Values.ToList())
+            //{
+            //    xmlser.Serialize(fs, order);
+            //}
+            xmlser.Serialize(fs, orderDict.Values.ToList());
+            fs.Close();
+            string xml = File.ReadAllText(fileName);
+            Console.WriteLine(xml);
+        }
+        public void XmlSerializeImport(XmlSerializer xmlser,string fileName)
+        {
+            orderDict.Clear();
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            List<Order> orders = xmlser.Deserialize(fs) as List<Order>;
+            foreach(Order order in orders)
+            {
+                AddOrder(order);
+            }
+        }
+        /// <summary>
+        /// add new order
+        /// </summary>
+        /// <param name="order">the order will be added</param>
+        public void AddOrder(Order order) {
+            if (orderDict.ContainsKey(order.Id))
+                throw new Exception($"order-{order.Id} is already existed!");
+            orderDict[order.Id] = order;
+        }
+
+        /// <summary>
+        /// cancel order
+        /// </summary>
+        /// <param name="orderId">id of the order which will be canceled</param> 
+        public void RemoveOrder(uint orderId) {
+              orderDict.Remove(orderId);
+        }
+
+        /// <summary>
+        /// query all orders
+        /// </summary>
+        /// <returns>List<Order>:all the orders</returns> 
+        public List<Order> QueryAllOrders() {
+            return orderDict.Values.ToList();
+        }
+
+        /// <summary>
+        /// query by orderId
+        /// </summary>
+        /// <param name="orderId">id of the order to find</param>
+        /// <returns>List<Order></returns> 
+        public Order GetById(uint orderId) {
+            return orderDict[orderId];
+        }
+
+        /// <summary>
+        /// query by goodsName
+        /// </summary>
+        /// <param name="goodsName">the name of goods in order's orderDetail</param>
+        /// <returns></returns> 
+        public List<Order> QueryByGoodsName(string goodsName) {
+            var query = orderDict.Values.Where(order =>
+                    order.Details.Where(d => d.Goods.Name == goodsName)
+                    .Count() > 0
+                );
+            return query.ToList();
+   
+        }
+
+        /// <summary>
+        /// query by customerName
+        /// </summary>
+        /// <param name="customerName">customer name</param>
+        /// <returns></returns> 
+        public List<Order> QueryByCustomerName(string customerName) {
+            var query=orderDict.Values
+                .Where(order => order.Customer.Name == customerName);
+            return query.ToList();
+        }
+
+        public List<Order> QueryByPrice(double price)
+        {
+            var query = orderDict.Values
+                .Where(order => order.Amount> price);
+            return query.ToList();
+        }
+
+
+        /// <summary>
+        /// edit order's customer
+        /// </summary>
+        /// <param name="orderId"> id of the order whoes customer will be update</param>
+        /// <param name="newCustomer">the new customer of the order which will be update</param> 
+        public void UpdateCustomer(uint orderId, Customer newCustomer) {
+            if (orderDict.ContainsKey(orderId)) {
+                orderDict[orderId].Customer = newCustomer;
+            } else {
+                throw new Exception($"order-{orderId} is not existed!");
+            }
+        }
+
+        /*other edit function with write in the future.*/
+    }
+}
---
+++ homework6/OrderTestTests/OrderServiceTests.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using ordertest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ordertest.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        //[TestMethod()]
+        //public void AddOrderTest()
+        //{
+        //    Assert.Fail();
+        //}
+        //正确添加订单
+        [TestMethod()]
+        public void AddOrderTest1()
+        {
+            Customer customer1 = new Customer(1, "Customer1");
+            Goods milk = new Goods(1, "Milk", 69.9);
+            OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(orderDetails3);
+
+            OrderService os = new OrderService();
+            os.AddOrder(order1);
+
+            Dictionary<uint, Order> ans = new Dictionary<uint, Order>();
+            ans[order1.Id] = order1;
+            CollectionAssert.AreEqual(ans, os.Dict);
+            Assert.AreEqual(os.Dict[order1.Id], order1);
+
+        }
+
+        //错误重复添加订单
+        [TestMethod()]
+        public void AddOrderTest2()
+        {
+            Customer customer1 = new Customer(1, "Customer1");
+            Goods milk = new Goods(1, "Milk", 69.9);
+            OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(orderDetails3);
+
+            OrderService os = new OrderService();
+            os.AddOrder(order1);
+            os.AddOrder(order1);
+
+        }
+
+        //正确删除订单
+        [TestMethod()]
+        public void RemoveOrderTest1()
+        {
+            Customer customer1 = new Customer(1, "Customer1");
+            Customer customer2 = new Customer(2, "Customer2");
+
+            Goods milk = new Goods(1, "Milk", 69.9);
+            Goods eggs = new Goods(2, "eggs", 4.99);
+            Goods apple = new Goods(3, "apple", 5.59);
+
+            OrderDetail orderDetails1 = new OrderDetail(1, apple, 800);
+            OrderDetail orderDetails2 = new OrderDetail(2, eggs, 2);
+            OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+
+            Order order1 = new Order(1, customer1);
+            Order order2 = new Order(2, customer2);
+            order1.AddDetails(orderDetails1);
+            order1.AddDetails(orderDetails2);
+            order1.AddDetails(orderDetails3);
+            order2.AddDetails(orderDetails2);
+            order2.AddDetails(orderDetails3);
+
+            OrderService os = new OrderService();
+            os.AddOrder(order1);
+            os.AddOrder(order2);
+            os.RemoveOrder(2);
+
+            Dictionary<uint, Order> ans = new Dictionary<uint, Order>();
+            ans[order1.Id] = order1;
+            CollectionAssert.AreEqual(os.Dict, ans);
+        }
+
+        //重复删除订单
+        [TestMethod()]
+        public void RemoveOrderTest2()
+        {
+            Customer customer1 = new Customer(1, "Customer1");
+            Customer customer2 = new Customer(2, "Customer2");
+
+            Goods milk = new Goods(1, "Milk", 69.9);
+            Goods eggs = new Goods(2, "eggs", 4.99);
+            Goods apple = new Goods(3, "apple", 5.59);
+
+            OrderDetail orderDetails1 = new OrderDetail(1, apple, 800);
+            OrderDetail orderDetails2 = new OrderDetail(2, eggs, 2);
+            OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+
+            Order order1 = new Order(1, customer1);
+            Order order2 = new Order(2, customer2);
+            order1.AddDetails(orderDetails1);
+            order1.AddDetails(orderDetails2);
+            order1.AddDetails(orderDetails3);
+            order2.AddDetails(orderDetails2);
+            order2.AddDetails(orderDetails3);
+
+            OrderService os = new OrderService();
+            //os.AddOrder(order1);
+            //os.AddOrder(order2);
+            os.RemoveOrder(order2.Id);
+            os.RemoveOrder(order2.Id);
+
+        }
+
+        //正确测试
+        [TestMethod()]
+        public void GetByIdTest1()
+        {
+            Customer customer1 = new Customer(1, "Customer1");
+            Goods milk = new Goods(1, "Milk", 69.9);
+            OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(orderDetails3);
+
+            OrderService os = new OrderService();
+            os.AddOrder(order1);
+
+            Order order = os.GetById(order1.Id);
+            Assert.AreEqual(order1, order);
+
+        }
+
+
+        //错误测试
+        [TestMethod()]
+        public void GetByIdTest2()
+        {
+            Customer customer1 = new Customer(1, "Customer1");
+            Goods milk = new Goods(1, "Milk", 69.9);
+            OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(orderDetails3);
+
+            OrderService os = new OrderService();
+            os.AddOrder(order1);
+
+            Order order = os.GetById(2);
+            Assert.AreEqual(order1, order);
+
+        }
+
+        //正确测试
+        [TestMethod()]
+        public void QueryByGoodsNameTest1()
+        {
+            Customer customer1 = new Customer(1, "Customer1");
+            Goods milk = new Goods(1, "Milk", 69.9);
+            OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(orderDetails3);
+
+            OrderService os = new OrderService();
+            os.AddOrder(order1);
+
+            List<Order> order = os.QueryByGoodsName("Milk");
+
+            Assert.AreEqual(order1, order[0]);
+        }
+
+
+
+        //错误测试
+        [TestMethod()]
+        public void QueryByGoodsNameTest2()
+        {
+            Customer customer1 = new Customer(1, "Customer1");
+            Goods milk = new Goods(1, "Milk", 69.9);
+            OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+            Order order1 = new Order(1, customer1);
+            order1.AddDetails(orderDetails3);
+
+            OrderService os = new OrderService();
+            os.AddOrder(order1);
+
+            List<Order> order = os.QueryByGoodsName("Milk1");
+
+            Assert.AreEqual(order1, order[0]);
+        }
+
+
+        [TestMethod()]
+        public void XmlSerializeImportTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
